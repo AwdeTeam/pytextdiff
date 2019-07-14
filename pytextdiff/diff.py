f1e078ddb4ae80819a0466720eaa5f2e2232c302
AwdeDarkar <darkardengeno@gmail.com>
2019-07-13 20:27:37 -0400

    Module doc example

---
+++ pytextdiff/diff.py
@@ -0,0 +1,17 @@
+"""
+diff
+====================================================================================================
+
+Diff object which can be applied to text.
+
+----------------------------------------------------------------------------------------------------
+
+**Created**
+    2019-07-13
+**Updated**
+    2019-07-13 by Darkar
+**Author**
+    Darkar
+**Copyright**
+    This software is licensed under GPL3, see LICENSE for details.
+"""
