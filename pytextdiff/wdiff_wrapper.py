0981de4adb12783bb84373771ac1110768e65694
Nathan Martindale <nathanamartindale@gmail.com>
2019-07-13 19:43:43 -0500

    Finish call_wdiff

--- pytextdiff/wdiff_wrapper.py
+++ pytextdiff/wdiff_wrapper.py
@@ -1,4 +1,3 @@
-
 """
 wdiff_wrapper
 ====================================================================================================
@@ -19,13 +18,26 @@ Functions to handle interfacing with the wdiff command line utility.
 
 import subprocess
 from shutil import which
+from shlex import split
 
 from pytextdiff.exceptions import WDiffNotFound
 
 
 def call_wdiff(original_text_file, updated_text_file):
-    """Call the command line utility."""
+    """
+    Call the command line utility.
+
+    Returns the string output from the wdiff command
+    """
 
     # check that the wdiff utility exists
     if which("wdiff") is None:
         raise WDiffNotFound()
+
+    cmd = "wdiff '{0}' '{1}'".format(original_text_file, updated_text_file)
+
+    # run the command and return the output
+    cmd_output_b = subprocess.run(split(cmd), stdout=subprocess.PIPE)
+    cmd_output = cmd_output_b.stdout.decode('utf-8')
+    
+    return cmd_output
